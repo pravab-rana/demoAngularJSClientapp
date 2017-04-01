b1df460aba954463fa8b09c5bf2d7ad5ad52555c
Pravab Rana <pravab.rana@gmail.com>
2017-04-01 18:01:22 -0400

    Added working login form validator

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -25,20 +25,39 @@ export class LoginComponent implements OnInit {
     password:""
   };
 
+  /*
+  This method calls the getUser method internally to retrieve user details 
+  from mongodb database calling the expressJS end-point
+  It then checks for matching username and password against the form entered values
+  */
   onLogin(loginform:NgForm) {
-    console.log('In login method');
+    console.log('onLogin method');
     console.log('User details:',loginform.value);
     this.getUsers();
+    if(this.httpdata!=null){
+        console.log('User no:'+this.httpdata.docs.length);
+        for(let doc of this.httpdata.docs){
+          console.log('Doc:',doc);
+          if(doc.username==loginform.value.username) {
+            console.log('Matching username found');
+            if(doc.password==loginform.value.password){
+              console.log('Credentials match');
+            }
+          }
+        }
+      }
+      
   }
 
+  /*
+  This method uses the actual LoginService's getUsers observable to get data from the 
+  expressJS end-point
+  */
   getUsers() {
     console.log('Calling loginService.getUsers()');
     this.loginService.getUsers()
       .subscribe((data: any) => this.httpdata = data,
        (error:any) => {this.errorMessage = error;console.log('Error:',this.errorMessage)});
-
       console.log('Data from loginService:',this.httpdata);
-  }
-
-  
+    }
 }
