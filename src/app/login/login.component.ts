5d93710a02069c17a0d37e2e3cfa482c6b4268d2
Pravab Rana <pravab.rana@gmail.com>
2017-04-02 11:23:25 -0400

    Feature to check for already logged in user added

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,8 +10,9 @@ import { LoginService } from '../login.service';
 export class LoginComponent implements OnInit {
   
   loginService:LoginService;
-  errorMessage:string;
+  message:string;
   httpdata:any;
+  isLoggedIn:boolean;
   
   constructor(loginService:LoginService){
      this.loginService = loginService;
@@ -33,6 +34,15 @@ export class LoginComponent implements OnInit {
   onLogin(loginform:NgForm) {
     console.log('onLogin method');
     console.log('User details:',loginform.value);
+
+    //checking if the user has already logged in
+    this.isLoggedIn = this.checkUserinLocalStorage(loginform.value.username);
+    if(this.isLoggedIn){
+      console.log('User already loggedin');
+      this.message="User already logged in";
+      return;
+    }
+
     this.getUsers();
     if(this.httpdata!=null){
         console.log('User no:'+this.httpdata.docs.length);
@@ -42,9 +52,7 @@ export class LoginComponent implements OnInit {
             console.log('Matching username found');
             if(doc.password==loginform.value.password){
               console.log('Credentials match');
-             
               var currentDtTm = new Date();
-              
               //getTime() returns the numeric value in milliseconds since Jan 1,1970
               //as such 60000 is 1 min, and I am setting the expiryDtTm to 5 mins
               var expirtyDtTm = new Date(currentDtTm.getTime() + 5*60000);
@@ -55,13 +63,36 @@ export class LoginComponent implements OnInit {
               //adding to localStorage for persistance
               localStorage.setItem('loggedusername',loginform.value.username);
               localStorage.setItem('expirydttm',expirtyDtTm.toString());
-             
+       
+              //match has been found so no further users need to checked
+              return;
               
-            }
-          }
-        }
-      }
+            }//end of if(doc.password==loginform.value.password){
+          }// end of if(doc.username==loginform.value.username)
+        }//end of for loop
+      }//end of if(this.httpdata!=null)      
+  }
+
+  checkUserinLocalStorage(username:string){
+    console.log('Checking localStorage');
+    if(username===localStorage.getItem('loggedusername')) {
+      console.log('Logged user Exists in localStorage');
+      var expiryDtTmString = localStorage.getItem('expirydttm');
       
+      //console.log('CurrentDtTm:'+new Date().getTime());
+      //console.log('ExpirtyDtTm:'+Date.parse(expiryDtTmString));
+       
+      var expirtyDtTmDate = Date.parse(expiryDtTmString);
+      if(expirtyDtTmDate>new Date().getTime()){
+        console.log('Login session has not expired');
+        return(true);
+      }
+      else{
+        console.log('Login session has expired');
+        return(false);
+      }
+    }
+    return(false);
   }
 
   /*
@@ -72,7 +103,7 @@ export class LoginComponent implements OnInit {
     console.log('Calling loginService.getUsers()');
     this.loginService.getUsers()
       .subscribe((data: any) => this.httpdata = data,
-       (error:any) => {this.errorMessage = error;console.log('Error:',this.errorMessage)});
+       (error:any) => {this.message = error;console.log('Error:',this.message)});
       console.log('Data from loginService:',this.httpdata);
     }
 }
